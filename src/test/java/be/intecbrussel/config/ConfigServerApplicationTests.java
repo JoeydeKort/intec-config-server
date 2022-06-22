d25b6a813be70b92de0e85ccfb27f9fb611e104f
JoeydeKort <dekort.jdk@gmail.com>
2022-06-22 15:56:23 +0200

    init

---
+++ src/test/java/be/intecbrussel/config/ConfigServerApplicationTests.java
@@ -0,0 +1,15 @@
+package be.intecbrussel.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("development")
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
